5c7ccdb7afe0eeeebd56c2d4ed5031f1ac6a9c62
Philip Larie <philiprlarie@gmail.com>
2018-11-11 12:14:13 -0800

    Add many to many relationship for ingredients and recipes
    https://guides.emberjs.com/release/models/defining-models/

--- app/models/recipe.js
+++ app/models/recipe.js
@@ -3,5 +3,8 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   source: DS.attr('string'),
-  steps: DS.attr('string')
+  steps: DS.attr('string'),
+
+  ingredients: DS.hasMany('ingredient'),
+  recipeIngredients: DS.hasMany('recipe-ingredient')
 })
