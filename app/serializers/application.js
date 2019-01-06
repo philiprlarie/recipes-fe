e265880cddd33e8f935d47e9db3932d5ea898985
Philip Larie <philiprlarie@gmail.com>
2019-01-06 13:54:58 -0800

    Fix bug where we were sending relationships from the front end that were deleting recipe-ingredients on the backend
    when we saved ingredients, it would send any relationships that existed on the front end (which depended on what data was loaded on the front end). When those relationships were included, any that weren't got deleted. E.g. we had honey as an ingredient in recipe 1 and 2. When recipe 1 was loaded, but recipe 2 wasn't, if we ever saved the honey ingredient, the recipe ingredient for honey on recipe 2 would be deleted.

---
+++ app/serializers/application.js
@@ -0,0 +1,12 @@
+import DS from 'ember-data';
+
+export default DS.JSONAPISerializer.extend({
+  serialize() {
+    let json = this._super(...arguments);
+
+    delete json.data.attributes['created-at'];
+    delete json.data.attributes['updated-at'];
+
+    return json;
+  }
+});
---
+++ app/serializers/ingredient.js
@@ -0,0 +1,11 @@
+import ApplicationSerializer from './application';
+
+export default ApplicationSerializer.extend({
+  serialize() {
+    let json = this._super(...arguments);
+
+    delete json.data.relationships
+
+    return json;
+  }
+});
